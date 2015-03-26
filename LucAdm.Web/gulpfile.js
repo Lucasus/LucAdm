e3b325577712ae351f3b31c2ac3364c0cc40f312
Lucasus <lukasz.wiatrak@gmail.com>
2015-03-26 23:04:21 +0100

    Different gulp builds for different configurations

--- LucAdm.Web/gulpfile.js
+++ LucAdm.Web/gulpfile.js
@@ -5,35 +5,68 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var del = require('del');
+var inject = require('gulp-inject');
+var series = require('stream-series')
+var debug = require('gulp-debug');
 
 var config = {
-   //Include all js files but exclude any min.js files
     lib: [
         'bower_components/angular/angular.js',
         'bower_components/angular-bootstrap/ui-bootstrap.js'],
-        //'app/**/*.js',
-        //'!app/**/*.min.js'],
-    css: ['bower_components/bootstrap/dist/css/bootstrap.css']
- }
+    src: ['app/**/*.js',
+        '!app/**/*.min.js',
+        '!app/all.js'],
+    css: ['bower_components/bootstrap/dist/css/bootstrap.css',
+        '!css/all.css'],
+}
  
-// Synchronously delete the output file(s)
 gulp.task('clean', function(){
-  del.sync(['app/all.min.js'])
+  del.sync(['app/all.js'])
 });
  
-// Combine and minify all files from the app folder
-gulp.task('scripts', ['clean'], function() {
- 
-  return gulp.src(config.src)
-//    .pipe(uglify())
-//    .pipe(concat('all.min.js'))
-    .pipe(gulp.dest('app/lib'));
+
+// Debug tasks:
+gulp.task('scripts-debug', ['clean'], function () {
+    return gulp.src(config.lib)
+      .pipe(gulp.dest('./lib'));
 });
 
-gulp.task('css', function () {
+gulp.task('css-debug', function () {
     return gulp.src(config.css)
-    .pipe(gulp.dest('app/css'));
+    .pipe(gulp.dest('./css'));
 });
- 
+
+gulp.task('index-debug', ['css-debug', 'scripts-debug'], function () {
+    return gulp.src('index.html')
+        .pipe(inject(series(gulp.src(['css/*.css', '!css/all.css']), gulp.src('lib/*.js'), gulp.src(config.src)).pipe(debug())))
+        .pipe(gulp.dest('.'));
+});
+
+// Release tasks
+
+gulp.task('scripts-release', ['clean'], function () {
+    return gulp.src(config.lib.concat(config.src))
+      .pipe(debug())
+      .pipe(concat('all.js'))
+      .pipe(gulp.dest('./app'));
+});
+
+
+gulp.task('css-release', function () {
+    return gulp.src(config.css)
+    .pipe(debug({title: 'css-release'}))
+    .pipe(concat('all.css'))
+    .pipe(gulp.dest('./css'));
+});
+
+
+gulp.task('index-release', ['css-release', 'scripts-release'], function () {
+    return gulp.src('index.html')
+        .pipe(inject(gulp.src(['css/all.css', 'app/all.js'])))
+        .pipe(gulp.dest('.'));
+});
+
 //Set a default tasks
-gulp.task('default', ['scripts', 'css'], function(){});
+gulp.task('debug', ['scripts-debug', 'css-debug', 'index-debug'], function () { });
+gulp.task('release', ['scripts-release', 'css-release', 'index-release'], function () { });
+
