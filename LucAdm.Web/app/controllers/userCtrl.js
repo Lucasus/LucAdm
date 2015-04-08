d8a5c3942d6d27350b01084e43d4ebb57ef362e5
Lucasus <lukasz.wiatrak@gmail.com>
2015-04-08 20:09:55 +0200

    List of users - better styling

--- LucAdm.Web/app/controllers/userCtrl.js
+++ LucAdm.Web/app/controllers/userCtrl.js
@@ -4,12 +4,22 @@
     angular.module('lucAdm').controller("UserCtrl", function ($scope, $http) {
 
         $http.get("api/user").success(function (data) {
-            var model = { list: data };
+            var model = {
+                list: data,
+                totalPages: 3,
+                total: 25,
+                page: 1,
+            };
+
             $scope.users = model;
 
             $scope.addUser = function (userName) {
                 model.list.push({ userName: userName, active: false });
-            }
+            };
+
+            $scope.pageChanged = function () {
+
+            };
         });
     });
 }());
