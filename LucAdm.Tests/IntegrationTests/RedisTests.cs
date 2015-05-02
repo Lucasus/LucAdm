7015e81262d3a7750c90cea1f97a765d94ce35fe
Lucasus <lukasz.wiatrak@gmail.com>
2015-05-02 18:57:37 +0200

    Added Redis test for experiments

---
+++ LucAdm.Tests/IntegrationTests/RedisTests.cs
@@ -0,0 +1,18 @@
+﻿using StackExchange.Redis;
+using Xunit;
+using FluentAssertions;
+
+namespace LucAdm.Tests
+{
+    public class RedisTests
+    {
+        [NamedFact]
+        [Trait("Category", "Integration-Experimental")]
+        public void Should_Correctly_Connect_To_Redis()
+        {
+            var redis = ConnectionMultiplexer.Connect("192.168.1.193:6379");
+
+            redis.IsConnected.Should().BeTrue();
+        }
+}
+    }
